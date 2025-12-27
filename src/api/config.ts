0edd01911da187c33e26a2d65668bbcf7f5e0002
amoghmunikote <amogh.munikote@gmail.com>
2025-12-26 18:21:22 -0800

    Removed double-slash

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-export const ATHENA_URL = 'https://api.konik.ai/ws/'
-export const API_URL = 'https://api.konik.ai/'
+export const ATHENA_URL = 'https://api.konik.ai/ws'
+export const API_URL = 'https://api.konik.ai'
 export const BILLING_URL = 'https://billing.comma.ai'
-export const USERADMIN_URL = 'https://useradmin.konik.ai/'
+export const USERADMIN_URL = 'https://useradmin.konik.ai'
